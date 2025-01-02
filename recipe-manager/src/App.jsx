736f5e52f64ba136db41f78eebdc18bb420aac2a
ayselpanahova-16022 <118384414+ayselpanahova-16022@users.noreply.github.com>
2025-01-03 03:24:15 +0400

    fixing overall wrapping style

--- recipe-manager/src/App.jsx
+++ recipe-manager/src/App.jsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RecipesOverviewPage from "./pages/RecipesOverviewPage";
 import RecipePage from "./pages/RecipePage";
-import CreateRecipePage from "./pages/CreateRecipePage";
-import ContactPage from "./pages/ContactPage"; // Import ContactPage
+import CreateRecipe from "./components/CreateRecipe"; // Update this import
+import ContactPage from "./pages/ContactPage";
 import NavBar from "./components/NavBar";
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]); // Add state for managing recipes
+
+  const handleRecipeAdded = (newRecipe) => {
+    // Update recipes state when a new recipe is added
+    setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
+  };
+
   return (
     <Router>
       <div style={{ display: "flex", height: "100vh" }}>
         {/* NavBar on the left */}
-        <div style={{ width: "200px", flexShrink: 0 }}>
           <NavBar />
-        </div>
 
         {/* Main content on the right */}
         <div style={{ flex: 1, padding: "20px", overflowY: "auto" }}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/recipes" element={<RecipesOverviewPage />} />
+            <Route path="/recipes" element={<RecipesOverviewPage recipes={recipes} />} />
             <Route path="/recipe/:id" element={<RecipePage />} />
-            <Route path="/create" element={<CreateRecipePage />} />
-            <Route path="/contact" element={<ContactPage />} /> {/* Add Contact Route */}
+            <Route
+              path="/create"
+              element={<CreateRecipe onRecipeAdded={handleRecipeAdded} />}
+            />
+            <Route path="/contact" element={<ContactPage />} />
           </Routes>
         </div>
       </div>
